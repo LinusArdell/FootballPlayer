ac22247b6a451f79f5af8dd1666a708304d60108
AmbroDwik <69383799+AmbroDwik@users.noreply.github.com>
2023-06-07 19:54:24 +0700

    Update create.blade.php

--- resources/views/negara/create.blade.php
+++ resources/views/negara/create.blade.php
@@ -1,76 +1,57 @@
-
-      @extends('layout.mainA')
-      @section('Judul')
+@extends('layout.mainA')
+@section('Judul')
           Daftar Pemain Bola
-      @endsection
-    @section('subJudul')
+@endsection
+@section('subJudul')
           Create List Negara
-      @endsection
+@endsection
 
-            @section('content')
+ @section('content')
 
-            <div class="row">
-          <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                  <div class="d-flex justify-contentobetween">
-                      <form class="col-lg-6 rounded border-info border d-flex" method="GET">
-                          <input type="text" name="search" class="form-control" placeholder="">
-                      </form>
-                  </div>
+<div class="col-md-6 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        @if (Session::get('success'))
+        <div class="alert alert-success">{{ Session::get('success') }}</div>
 
+        @else
 
-                  {{-- @if (count($pemain)!=0)<
-                      <button class="btn btn-danger" id="multi-delete" data-route="{{ route('mhs-multi-delete') }}">Delete All Selected</button>
-                  @endif --}}
-
-<!-- ============================================================== -->
-<!--                    Batas Suci          content Showing -->
-                <div class="table-responsive">
-                  <table class="table table-striped">
-                      <thead>
-                          <tr>
-
-                              <th>Nama Pemain</th>
-                              <th>nomor Punggung</th>
-                              <th>Posisi</th>
-                              <th>Foto</th>
-                              <th>Klub Saat Ini</th>
-                              <th>Asal Negara</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          {{-- @foreach ($pemain as $item)
-                          <tr>
-                              <td> {{ $item->nama }} </td>
-                              <td> {{ $item->nomor_punggung }} </td>
-                              <td> {{ $item->posisi }} </td>
-                              <td> {{ $item->klub_id}} </td>
-                              <td> <img src="{{ $item->foto ? asset('storage/'.$item->foto) : asset('images/faces/face5.jpg') }}">  </td>
-                              <td> {{ $item->negara }}</td>
-                              <td> {{ $item->created_at }} </td>
-                              <td>
-                                  <div></div>
-                                  <a href="{{ route('pemain.edit',$item->id) }}">
-                                  <button class ="btn btn-success btn-sm">Edit</button></a>
-                                  <form method="post" class="delete-form" data-route="{{ route('mahasiswa.destroy',$item->id) }}">
-                                      @method('delete')
-                                      @csrf
-                                      <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                  </form>
-                              </td>
-                          </tr>
-                  @endforeach --}}
-                      </tbody>
-                  </table>
-                  {{-- <div class = "mt-4">{{ $mahasiswas ->withQueryString()->links('pagination::bootstrap-5') }}</div> --}}
-                </div>
-
-                <!--                    Batas Suci          content Showing -->
-                <!-- ============================================================== -->
-              </div>
-            </div>
-          </div>
+        @endif
+        <h4 class="card-title">Input Negara to List</h4>
+        <p class="card-description">
+          Tambah List baru
+        </p>
+        <form class="forms-sample" action="{{ route('negara.store') }}" method="POST">
+          @csrf
         </div>
+    </div>
+        <div class="card">
+            <div class="card-body">
+
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nama negara</label>
+            <input type="text" class="form-control" name="negara" placeholder="Nama Negara">
+            @error('negara')
+            <span class="text-danger">{{ $message }}</span>
+            @enderror
+
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Bendera</label>
+            <input type="text" class="form-control" name="bendera" placeholder="Bendera">
+
+            @error('bendera')
+            <span class="text-danger">{{ $message }}</span>
+            @enderror
+
+          </div>
+          <button type="submit" class="btn btn-primary me-2">Submit</button>
+          <a href="{{ route('pemain.index') }}" class="btn btn-light">Cancel</button>
+        </form>
+    </div>
+    </div>
+  </div>
+
 
               @endsection
